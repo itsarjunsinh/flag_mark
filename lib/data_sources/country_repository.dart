da347340a40d638921972213f0a1a41ad82a3e7b
itsarjunsinh <mrarjunsinh@gmail.com>
2021-01-25 09:02:24 +0530

    Remove debug message

--- lib/data_sources/country_repository.dart
+++ lib/data_sources/country_repository.dart
@@ -31,7 +31,6 @@ class CountryRepository {
         allCountries.add(
           Country(countryCode, countryInfo.country, countryInfo.region),
         ),
-        print(countryInfo.country)
       },
     );
 
